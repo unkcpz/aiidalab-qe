bbc59343f0677c6f9b31cb8c322aa3517c26c512
Simon Adorf <simon@adorf.epfl.ch>
2021-03-03 16:54:43 +0000

    Update for adjusted payload schema.

--- query_importer.py
+++ query_importer.py
@@ -1,3 +1,6 @@
+import base64
+import hashlib
+import json
 import tempfile
 from urllib.parse import urlsplit, parse_qs
 
@@ -6,6 +9,7 @@ import traitlets
 from aiida.orm import Dict
 from aiida.orm import StructureData
 from aiidalab_widgets_base.utils import get_ase_from_file
+from cryptography.fernet import Fernet
 
 
 class QueryImporter(traitlets.HasTraits):
@@ -13,23 +17,38 @@ class QueryImporter(traitlets.HasTraits):
     structure = traitlets.Instance(StructureData, allow_none=True)
     parameters = traitlets.Instance(Dict, allow_none=True)
 
-    def _request_and_parse_input(self, input_query):
-        response = requests.get(input_query)
+    def _request_and_parse_input(self, input_url, key, sha256):
+        response = requests.get(input_url)
         response.raise_for_status()
-        input_ = response.json()
-        with tempfile.NamedTemporaryFile(suffix=".xsf") as tmp_file:
-            tmp_file.write(input_["structure"].encode("utf-8"))
+
+        decrypted_content = Fernet(key).decrypt(response.content)
+        assert hashlib.sha256(decrypted_content).hexdigest() == sha256
+
+        return json.loads(decrypted_content)
+
+    def _set_input(self, input_):
+        assert input_["structure"]["format"] == "xyz"
+        structure_data = base64.standard_b64decode(input_["structure"]["data"])
+
+        with tempfile.NamedTemporaryFile(suffix=".xyz") as tmp_file:
+            tmp_file.write(structure_data)
             tmp_file.flush()
             atoms = get_ase_from_file(tmp_file.name)
             self.set_trait("structure", StructureData(ase=atoms))
+
         self.set_trait("parameters", Dict(dict=input_["inputs_dict"]))
 
     def from_callback(self, url):
         with self.hold_trait_notifications():
             try:
                 query = parse_qs(urlsplit(url).query)
-                input_query = query["input"][0]
+                kwargs = dict(
+                    input_url=query["input"][0],
+                    key=query["key"][0],
+                    sha256=query["sha256"][0],
+                )
             except (KeyError, IndexError):
                 pass
             else:
-                self._request_and_parse_input(input_query)
+                input_ = self._request_and_parse_input(**kwargs)
+                self._set_input(input_)
